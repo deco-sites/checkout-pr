6154549b2e53b8f5dd611bf6a8e66ad33a9a87fa
Guilherme Lima Desoler <guilherme.desoler@primum.edu.br>
2025-07-07 23:30:33 -0300

    Add Header component with customizable image and buttons

---
+++ sections/Header.tsx
@@ -0,0 +1,33 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+
+interface Props {
+  image: string;
+  alt?: string;
+  height?: number;
+  width?: number;
+  buttons?: {
+    text: string;
+    href: string;
+  }[];
+}
+
+export default function Header({
+  image = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
+  alt = "Made with deco.cx",
+  height = 20,
+  width = 50,
+  buttons = [],
+}: Props) {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", padding: "8px 16px" }}>
+      {image && (
+          <Image
+            src={image || ""}
+            alt={alt || ""}
+            height={height || 20}
+            width={width || 50}
+          />
+        )}
+    </div>
+  );
+}
