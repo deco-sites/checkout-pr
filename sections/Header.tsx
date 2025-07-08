16db7929737251c3743ce9377cc54cdb7fb9cf35
Guilherme Lima Desoler <guilherme.desoler@primum.edu.br>
2025-07-07 23:54:48 -0300

    Rename Header component to Section and remove unused Headertsx file

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -12,7 +12,7 @@ interface Props {
   }[];
 }
 
-export default function Header({
+export default function Section({
   image = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
   alt = "Made with deco.cx",
   height = 20,
